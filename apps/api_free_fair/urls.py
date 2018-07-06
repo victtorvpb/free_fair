8731baf030404c358b0e4a108d5ffe4e1465f96f
Victor Pereira <victtor.vpb@gmail.com>
2018-07-06 09:17:41 -0300

    fix path url api

--- apps/api_free_fair/urls.py
+++ apps/api_free_fair/urls.py
@@ -5,6 +5,6 @@ from .apiviews import FreeFairApiView
 app_name = 'api_free_fair'
 
 router = routers.DefaultRouter()
-router.register(r'v1/api_free_fair', FreeFairApiView, base_name='FreeFair')
+router.register(r'api_free_fair', FreeFairApiView, base_name='FreeFair')
 
 urlpatterns = router.urls
