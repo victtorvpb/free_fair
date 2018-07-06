ebbb6461b9e40b7bfe0b6a8c1d23e855ebf2dd47
Victor Pereira <victtor.vpb@gmail.com>
2018-07-05 21:33:52 -0300

    create model FreeFairModels

--- apps/api_free_fair/models.py
+++ apps/api_free_fair/models.py
@@ -1,3 +1,23 @@
+from django_extensions.db.models import TimeStampedModel
 from django.db import models
 
-# Create your models here.
+
+class FreeFairModels(TimeStampedModel):
+
+    id_file = models.IntegerField(blank=False, unique=True, null=False)
+    longitude = models.BigIntegerField(blank=False, null=False)
+    latitude = models.BigIntegerField(blank=False, null=False)
+    setcens = models.BigIntegerField(blank=False, null=False)
+    areap = models.BigIntegerField(blank=False, null=False)
+    coddist = models.IntegerField(blank=False,  null=False)
+    district = models.CharField(null=False, blank=False, max_length=50)
+    codsubpref = models.IntegerField(blank=False,  null=False)
+    subpref = models.CharField(null=False, blank=False, max_length=100)
+    region_five = models.CharField(null=False, blank=False, max_length=15)
+    region_eigth = models.CharField(null=False, blank=False, max_length=15)
+    name_fair = models.CharField(null=False, blank=False, max_length=100)
+    register = models.CharField(null=False, blank=False, max_length=100, unique=True)
+    address = models.CharField(null=False, blank=False, max_length=100)
+    number = models.CharField(null=False, blank=False, max_length=10)
+    neighborhood = models.CharField(null=False, blank=False, max_length=50)
+    reference_point = models.CharField(null=False, blank=False, max_length=150)
--- apps/api_free_fair/tests/tests_model.py
+++ apps/api_free_fair/tests/tests_model.py
@@ -1,5 +1,28 @@
 from django.test import TestCase
+from apps.api_free_fair.models import FreeFairModels
 
 
 class TestsFreeFairModel(TestCase):
-    pass
+
+    def setUp(self):
+        self.insert_free_fair = FreeFairModels.objects.create(id_file=1,
+                                                              longitude=-46550164,
+                                                              latitude=-23558733,
+                                                              setcens=355030885000091,
+                                                              areap=3550308005040,
+                                                              coddist=87,
+                                                              district='VILA FORMOSA',
+                                                              codsubpref=26,
+                                                              subpref='ARICANDUVA-FORMOSA-CARRAO',
+                                                              region_five='Leste',
+                                                              region_eigth='Leste1',
+                                                              name_fair='VILA FORMOSA',
+                                                              register='4041-0',
+                                                              address='RUA MARAGOJIPE',
+                                                              number='S/N',
+                                                              neighborhood='VL FORMOSA',
+                                                              reference_point='TV RUA PRETORIA')
+
+    def test_insert_free_fair(self):
+        free_fair_get = FreeFairModels.objects.get(id_file = 1)
+        self.assertEqual(self.insert_free_fair,free_fair_get )
